24ddf1c43c6e58f548dd9f6834de5a5e47c6c321
brianshkim <brianshkim25@gmail.com>
2023-12-21 04:30:19 -0800

    finished

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@ import cookieParser from 'cookie-parser'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js';
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
+
 
 dotenv.config()
 import connectDB from './config/db.js'
-import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 
 const port = process.env.PORT || 5000;
 
@@ -26,6 +28,7 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req,res) => res.send({clientId: process.env.PAYPAL_CLIENT_ID}))
 
